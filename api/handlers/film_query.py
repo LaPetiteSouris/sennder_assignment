1139f3831011a9f109f13c195bee516bc9324b86
Tung Hoang <sontunghoang@gmail.com>
2020-08-15 14:17:34 +0200

    build film index

--- api/handlers/film_query.py
+++ api/handlers/film_query.py
@@ -4,10 +4,39 @@ from utils import logger
 log = logger.define_logger(__name__)
 
 
-def join_movies_and_people(movies, people):
+def get_film_id_from_url(url):
+    return url.rsplit('films/', 1).pop()
+
+
+def build_film_index(film_data):
+    films_to_people_index = {}
+    for film in film_data:
+        films_to_people_index[film.get("id")] = film
+    return films_to_people_index
+
+
+def build_revert_index_from_people_to_movie(people_data, film_data):
+    films_to_people_index = {}
+    for film in film_data:
+        films_to_people_index[film.get("id")] = film
+
+    for person in people_data:
+        # person_data = {"id": person.get("id"), "name": person.get("name")}
+        involved_films = person.get("films", [])
+        for film_url in involved_films:
+            film_id = get_film_id_from_url(film_url)
+
+            # build reverted indexation from film to people
+            # if film exists in film_data
+            if film_data.get(film_id):
+                # append person data to film
+                return
+
+
+def join_movies_and_people(films, people):
     return {}
 
 
-def on_film_request(req_body):
+def on_movie_request(req_body):
     # Parse info from request body
     return "Hello"
--- test/test_parse_all_movies.py
+++ test/test_parse_all_movies.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from api.handlers.film_query import join_movies_and_people
+from api.handlers.film_query import join_movies_and_people, \
+    get_film_id_from_url, build_film_index
 
 sample_get_all_films_response = [{
-    "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+    "id": "if_of_film_one_piece",
     "title": "One Piece",
     "description": "Kaizoku Ou ni, ore wa naru",
     "director": "Eiichiro Oda",
@@ -10,7 +11,7 @@ sample_get_all_films_response = [{
     "release_date": "2010",
     "rt_score": "95"
 }, {
-    "id": "2baf70d1-42bb-4437-b551-efghiklm",
+    "id": "id_of_film_harry_potter",
     "title": "Harry Potter",
     "description": "Witch craft",
     "director": "J.K Rowling",
@@ -21,7 +22,7 @@ sample_get_all_films_response = [{
 
 sample_get_all_people_response = [{
     "id":
-    "ba924631-068e-4436-b6de-f3283fa848f0",
+    "id_of_monkey_dluffy",
     "name":
     "Monkey D. Luffy",
     "gender":
@@ -32,12 +33,10 @@ sample_get_all_people_response = [{
     "brown",
     "hair_color":
     "brown",
-    "films": [
-        "https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe"
-    ],
+    "films": ["https://ghibliapi.herokuapp.com/films/if_of_film_one_piece"],
 }, {
     "id":
-    "ba924631-068e-4436-b6de-f3283fa848g7",
+    "id_of_vinsmoke_sanji",
     "name":
     "Vinsmoke Sanji",
     "gender":
@@ -48,12 +47,10 @@ sample_get_all_people_response = [{
     "brown",
     "hair_color":
     "brown",
-    "films": [
-        "https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe"
-    ],
+    "films": ["https://ghibliapi.herokuapp.com/films/if_of_film_one_piece"],
 }, {
     "id":
-    "ba924631-068e-4436-b6de-f3283fa848f9",
+    "id_of_harry_potter",
     "name":
     "Harry Potter",
     "gender":
@@ -64,45 +61,44 @@ sample_get_all_people_response = [{
     "brown",
     "hair_color":
     "brown",
-    "films":
-    ["https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-efghiklm"],
+    "films": ["https://ghibliapi.herokuapp.com/films/id_of_film_harry_potter"],
 }]
 
 
-def _search_for_person_in_list(person, list_of_people):
-    try:
-        return next(item for item in list_of_people if item["name"] == person)
-    except StopIteration:
-        return None
+def test_get_movie_id_from_url():
+    # Given an url to person
+    url_movie = "https://ghibliapi.herokuapp.com/films/if_of_film_one_piece"
+
+    # Expect the film ID to be parsed correctly
+    movie_id = get_film_id_from_url(url_movie)
+    assert movie_id == "if_of_film_one_piece"
 
 
-def test_search_for_person_in_list():
-    # Given a list of people Vinsmoke Sanji and Monkey D. Luffy
-    list_of_people = [{
-        "id": "ba924631-068e-4436-b6de-f3283fa848f0",
-        "name": "Monkey D. Luffy",
-    }, {
-        "id": "a924631-068e-4436-b6de-f3283fa848g7",
-        "name": "Vinsmoke Sanji",
-    }]
-
-    # Search for Luffy
-    person = _search_for_person_in_list("Monkey D. Luffy", list_of_people)
-    assert person == {
-        "id": "ba924631-068e-4436-b6de-f3283fa848f0",
-        "name": "Monkey D. Luffy",
+def test_build_film_index():
+    # given sample film data
+    # build hash table refer to film ID
+    index = build_film_index(sample_get_all_films_response)
+    expected_results = {
+        "id_of_film_harry_potter": {
+            "id": "id_of_film_harry_potter",
+            "title": "Harry Potter",
+            "description": "Witch craft",
+            "director": "J.K Rowling",
+            "producer": "Hollywood",
+            "release_date": "2010",
+            "rt_score": "95"
+        },
+        "if_of_film_one_piece": {
+            "id": "if_of_film_one_piece",
+            "title": "One Piece",
+            "description": "Kaizoku Ou ni, ore wa naru",
+            "director": "Eiichiro Oda",
+            "producer": "Tohei Animation",
+            "release_date": "2010",
+            "rt_score": "95"
+        }
     }
-
-    # Search for Sanji
-    person = _search_for_person_in_list("Vinsmoke Sanji", list_of_people)
-    assert person == {
-        "id": "a924631-068e-4436-b6de-f3283fa848g7",
-        "name": "Vinsmoke Sanji",
-    }
-
-    # Search for Mr.Totoro (not in list)
-    person = _search_for_person_in_list("Mr.Totoro", list_of_people)
-    assert person is None
+    assert index == expected_results
 
 
 def test_join_movies_with_people():
@@ -115,64 +111,55 @@ def test_join_movies_with_people():
     joined_movies_people_data = join_movies_and_people(
         sample_get_all_films_response, sample_get_all_people_response)
 
-    # Expect that Vinsmoke Sanji and Monkey D.Luffy are in One Piece movie
-    # Harry Potter is in Harry Potter movie
+    # Expect that Vinsmoke Sanji and Monkey D.Luffy are in One Piece film
+    # Harry Potter is in Harry Potter film
 
     # Expected result should be in form of a dict (hashtable)
-    # with key is the ID of movie and content is the movie data
+    # with key is the ID of film and content is the film data
     # included related people
     expected_results = {
-        "2baf70d1-42bb-4437-b551-e5fed5a87abe": {
-            "id":
-            "2baf70d1-42bb-4437-b551-e5fed5a87abe",
-            "title":
-            "One Piece",
-            "description":
-            "Kaizoku Ou ni, ore wa naru",
-            "director":
-            "Eiichiro Oda",
-            "producer":
-            "Tohei Animation",
-            "release_date":
-            "2010",
-            "rt_score":
-            "95",
+        "if_of_film_one_piece": {
+            "id": "if_of_film_one_piece",
+            "title": "One Piece",
+            "description": "Kaizoku Ou ni, ore wa naru",
+            "director": "Eiichiro Oda",
+            "producer": "Tohei Animation",
+            "release_date": "2010",
+            "rt_score": "95",
             # Expect field people to be filled with
             # Monkey D.Luffy and Vinsmoke Sanji
-            "people": [{
-                "id": "ba924631-068e-4436-b6de-f3283fa848f0",
-                "name": "Monkey D. Luffy",
-            }, {
-                "id": "a924631-068e-4436-b6de-f3283fa848g7",
-                "name": "Vinsmoke Sanji",
-            }]
+            "people": {
+                "id_of_monkey_dluffy": {
+                    "id": "id_of_monkey_dluffy",
+                    "name": "Monkey D. Luffy",
+                },
+                "id_of_vinsmoke_sanji": {
+                    "id": "id_of_vinsmoke_sanji",
+                    "name": "Vinsmoke Sanji",
+                }
+            }
         },
-        "2baf70d1-42bb-4437-b551-efghiklm": {
-            "id":
-            "2baf70d1-42bb-4437-b551-efghiklm",
-            "title":
-            "Harry Potter",
-            "description":
-            "Witch craft",
-            "director":
-            "J.K Rowling",
-            "producer":
-            "Hollywood",
-            "release_date":
-            "2010",
-            "rt_score":
-            "95",
+        "id_of_film_harry_potter": {
+            "id": "id_of_film_harry_potter",
+            "title": "Harry Potter",
+            "description": "Witch craft",
+            "director": "J.K Rowling",
+            "producer": "Hollywood",
+            "release_date": "2010",
+            "rt_score": "95",
             # Expect field people to be filled with
             # Harry Potter
-            "people": [{
-                "id": "ba924631-068e-4436-b6de-f3283fa848f9",
-                "name": "Harry Potter",
-            }]
+            "people": {
+                "id_of_harry_potter": {
+                    "id": "id_of_harry_potter",
+                    "name": "Harry Potter",
+                }
+            }
         }
     }
 
     # Expect the joined data set to contain identical amount of information
-    # with expected results (e.g : there should be 2 and ONLY 2 movies,
+    # with expected results (e.g : there should be 2 and ONLY 2 films,
     # one of them is Harry Potter and the other is One Piece)
     assert set(joined_movies_people_data.keys()) == set(
         expected_results.keys())
@@ -180,31 +167,31 @@ def test_join_movies_with_people():
     # Expect field people to be filled with
     # Monkey D.Luffy and Vinsmoke Sanji
     # Search for Luffy
-    list_of_people = joined_movies_people_data.get(
-        "2baf70d1-42bb-4437-b551-e5fed5a87abe", {}).get("people")
+    set_of_people = joined_movies_people_data.get("if_of_film_one_piece",
+                                                  {}).get("people")
 
-    person = _search_for_person_in_list("Monkey D. Luffy", list_of_people)
+    person = set_of_people.get("id_of_monkey_dluffy")
     # Luffy should be in the list
     assert person == {
-        "id": "ba924631-068e-4436-b6de-f3283fa848f0",
+        "id": "id_of_monkey_dluffy",
         "name": "Monkey D. Luffy",
     }
 
     # Search for Sanji
-    person = _search_for_person_in_list("Vinsmoke Sanji", list_of_people)
+    person = set_of_people.get("id_of_vinsmoke_sanji")
     # Sanji should be in the list
     assert person == {
-        "id": "a924631-068e-4436-b6de-f3283fa848g7",
+        "id": "id_of_vinsmoke_sanji",
         "name": "Vinsmoke Sanji",
     }
 
     # Expect field people to be filled with
     # Harry Potter
-    list_of_people = joined_movies_people_data.get(
-        "2baf70d1-42bb-4437-b551-efghiklm", {}).get("people")
-    person = _search_for_person_in_list("Harry Potter", list_of_people)
+    set_of_people = joined_movies_people_data.get("id_of_film_harry_potter",
+                                                  {}).get("people")
+    person = set_of_people.get("id_of_harry_potter")
     # Mr.Potter should be in the list
     assert person == {
-        "id": "ba924631-068e-4436-b6de-f3283fa848f9",
+        "id": "id_of_harry_potter",
         "name": "Harry Potter",
     }
